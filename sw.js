57837ce8da92c98b04f8e29d877d2108ee68df21
Yu Kitsune <kitsunegadget@gmail.com>
2022-07-11 21:59:23 +0900

    Add updateBadges for setBadgeText & setTitle

--- sw.js
+++ sw.js
@@ -49,10 +49,10 @@ function updateDiscardsCount(notify = false, discarded = 0) {
   chrome.tabs.query({}, (tabs) => {
     const discards = tabs.filter((tab) => tab.discarded === true).length;
     //console.log(discards);
-    chrome.action.setBadgeText({ text: discards.toString() });
-    chrome.action.setTitle({
-      title: "Now discarding " + discards.toString() + " of " + tabs.length,
-    });
+    // chrome.action.setBadgeText({ text: discards.toString() });
+    // chrome.action.setTitle({
+    //   title: "Now discarding " + discards.toString() + " of " + tabs.length,
+    // });
 
     if (notify) {
       const notifyOption = {
@@ -70,4 +70,72 @@ function updateDiscardsCount(notify = false, discarded = 0) {
       chrome.notifications.create((NotificationOptions = notifyOption));
     }
   });
+}
+
+// test
+async function updateBadges(tabId) {
+  const tab = await chrome.tabs.get(tabId);
+  if (!tab.url) return;
+
+  const heapSize = await getTotalJSHeapSize(tab.id);
+
+  chrome.action.setBadgeText({
+    text: addSIunit(heapSize),
+    tabId: tab.id,
+  });
+
+  chrome.action.setTitle({
+    title: `Total JS heap size: ${heapSize} bytes`,
+    tabId: tab.id,
+  });
+}
+
+async function getTotalJSHeapSize(tabId) {
+  const results = await chrome.scripting.executeScript({
+    target: { tabId: tabId },
+    func: () => {
+      return performance.memory.totalJSHeapSize;
+    },
+  });
+
+  for (const frameResult of results) {
+    console.log(frameResult);
+    return frameResult.result;
+  }
+}
+
+function addSIunit(num) {
+  let text = "";
+  if (typeof num === "number") {
+    text = num.toString();
+  } else if (typeof num === "string") {
+    text = num;
+  } else {
+    throw new TypeError("num is not a number or string");
+  }
+
+  const unitMod = text.length % 3;
+
+  const unitPoint = (t, mod) => {
+    switch (mod) {
+      case 1:
+        return `${t.slice(0, 1)}.${t.slice(1, 3)}`;
+      case 2:
+        return `${t.slice(0, 2)}`;
+      case 0:
+        return `${t.slice(0, 3)}`;
+    }
+  };
+
+  // 123456 -> 123k
+  // 1234567 -> 1.2M
+  if (text.length <= 3) {
+    return text;
+  } else if (text.length <= 6) {
+    return `${unitPoint(text, unitMod)}K`;
+  } else if (text.length <= 9) {
+    return `${unitPoint(text, unitMod)}M`;
+  } else if (text.length <= 12) {
+    return `${unitPoint(text, unitMod)}G`;
+  }
 }
